a45f31f379859e180f88c69663f045a7d70e42c3
wang yongjie <yongjie.wang@ntu.edu.sg>
2018-11-20 15:52:07 +0800

    update install file

--- install-caffe.sh
+++ install-caffe.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Author: Yongjie Wang
-# Email: wangyongjie@ict.ac.cn
+# Email: yongjie.wang@ntu.edu.sg
 # This script is writen to install caffe on Ubuntu 16.04
 
 
 # install general dependencies
 
+sudo apt-get update
+sudo apt-get install -y build-essential cmake git pkg-config
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get install -y --no-install-recommends libboost-all-dev
 
@@ -20,6 +22,30 @@ sudo apt-get install -y libatlas-base-dev
 sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
 
 # git clone caffe
+cd third
 git clone https://github.com/BVLC/caffe.git
+cd caffe
 
 # modify Makefile.config
+cp ../../Makefile.config .
+
+make -j
+make pycaffe -j
+# if you want to install caffe which support matlab, uncomment this line below
+# make matcaffe -j
+
+set Pycaffe='pwd'/python
+echo Pycaffe>>~/.bashrc
+source ~/.bashrc
+
+# update numpy, protobuf, matplotlib
+
+pip install --upgrade numpy
+pip install --upgrate protobuf
+pip install --upgrade matplotlib
+
+# if install caffe successfully, this command return nothing
+python -c 'import caffe'
+
+# if it returns "no module named 'caffe' ", you have to google the error code to find the solutions or  send email to yongjie.wang@ntu.edu.sg
+
